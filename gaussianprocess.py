1c7831afee22e313556a7136e6c66167667bb7b1
atticusdrex <atticusdrex@gmail.com>
2025-04-15 16:11:59 -0400

    Removing some redundancies and polishing live script

--- gaussianprocess.py
+++ gaussianprocess.py
@@ -29,6 +29,28 @@ def rbf_kernel(x1, x2, kernel_params):
 
     return jnp.exp(-jnp.sum(h**2 / kernel_params))
 
+# Define the kernel function (e.g., RBF kernel)
+def laplace_kernel(x1, x2, kernel_params):
+    """
+    Radial Basis Function (RBF) kernel.
+
+    Parameters
+    ----------
+    x1, x2: array_like
+        two d-dimensional vectors. 
+
+    kernel_params: array_like
+        a d-dimensional vector specifying the diagonals of a 
+        covariance matrix which is inverted. 
+    
+    Returns: 
+    ---------
+    a scalar evaluating the RBF kernel at these two inputs 
+    """
+    h = (x1-x2).ravel()
+
+    return jnp.exp(-jnp.sum(np.abs(h) / kernel_params))
+
 # Gaussian Process Regression Class 
 def K(X1, X2, kernel_func, kernel_params):
     '''
